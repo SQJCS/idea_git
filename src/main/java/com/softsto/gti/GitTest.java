325c1609567aa21a3154ffc77ce1a9c385f9381e
sjs <1169115476@qq.com>
2023-05-31 14:41:20 +0800

    first commit

---
+++ src/main/java/com/softsto/gti/GitTest.java
@@ -0,0 +1,16 @@
+package com.softsto.gti;
+
+/**
+ * <p>git测试
+ *
+ * @author: shujiushan
+ * @version: 1.0
+ * @since: 2023/5/31 14:35
+ */
+public class GitTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+    }
+}
